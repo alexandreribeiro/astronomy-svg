5d164853d0b91366bc757b3efb45223f428e21b1
Alexandre Ribeiro <aribeiro2@gmail.com>
2025-06-01 22:40:45 +0200

    Include index.d.ts file

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+import { AstronomyJS } from "astronomyjs";
+
+export class AstronomySVG {
+    astronomyJS: AstronomyJS;
+
+    constructor();
+
+    getDate(): Date;
+
+    setDate(newDate: Date): void;
+
+    setLocation(latitude: number, longitude: number): void;
+
+    static initialize(latitude: number, longitude: number): AstronomySVG;
+
+    drawAzimuth(celestialBody: string, width: number): string;
+
+    drawAltitude(celestialBody: string, width: number): string;
+
+    drawSunAltitudePath(width: number, isRectangular?: boolean): string;
+
+    drawCelestialBodyRiseTime(celestialBody: string, width: number): string;
+
+    drawCelestialBodySettingTime(celestialBody: string, width: number): string;
+
+    drawCelestialBodyVisibility(celestialBody: string, width: number): string;
+
+    drawCelestialBodyVisibilityMap(
+        celestialBody: string,
+        width: number,
+        azimuthReference: number
+    ): string;
+
+    drawMultiCelestialBodyVisibilityMap(
+        celestialBodyList: string[],
+        width: number,
+        azimuthReference: number
+    ): string;
+}
